ea49434a5d01f67a39602ed4fa3188d830c538bf
Rim Goundal <goundalrim@gmail.com>
2023-02-16 15:17:02 -0800

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main -print quote
+ *
+ * Description: Printing Dora's quote
+ *
+ * Return: 1 (Success)
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
