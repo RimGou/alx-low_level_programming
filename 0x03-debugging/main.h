6d1a7ca9bd5ff010a73ca1e10f4ac17a27302759
Rim Goundal <goundalrim@gmail.com>
2023-02-21 13:49:45 -0800

    Task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * File: main.h
- * function prototype
- */
-int main(void);
+
 #include <stdio.h>
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
