4ae98dc18cd6814875193e8eefa02047199f8888
Rim Goundal <goundalrim@gmail.com>
2023-02-21 13:41:55 -0800

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 /**
- * main - test that the function positive_or_negative() gives the correct
- * output when given a case of 0.
+ * main - test that the function is positive or negative
  *Return: 0
  */
+
 int main(void)
 {
     int i;
