a69a59c7ab89b722e680f403f77d4e466c8e21c9
Rim Goundal <goundalrim@gmail.com>
2023-02-21 06:52:11 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,16 @@
  * Return: 0
  */
 
-int _putchar(_putchar)
+int main(void)
 {
-  return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
