1ff4bbc3d49ac400b61afe006a0ef038cd51eb07
Rim Goundal <goundalrim@gmail.com>
2023-02-21 10:23:24 -0800

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
  * _islower - Entry point
- * @n: input char
+ * @c: input char
  * Description: checks if char islower
  * Return: 1 if is lowercase else 0
  */
-int _islower(int n)
+int _islower(int c)
 {
 	char d;
 
 	for (d = 'a'; d <= 'z'; d++)
 	{
-		if (d == n)
+		if (d == c)
 			return (1);
 	}
 	return (0);
