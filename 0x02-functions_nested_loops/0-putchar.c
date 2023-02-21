75f670f8ad192a6e71c655465e6e619ea75ccd84
Rim Goundal <goundalrim@gmail.com>
2023-02-21 06:58:05 -0800

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -8,14 +8,15 @@
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	int i;
+	char c[]= "_putchar";
+
+	i = 0;
+	while(c[i])
+	{
+		_putchar(c[i]);
+		i++;
+	}
 	_putchar('\n');
 	return (0);
 }
