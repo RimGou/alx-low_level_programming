2520be2ef742b5149f7779edc564dea6abb7b72b
Rim Goundal <goundalrim@gmail.com>
2023-02-23 14:54:39 -0800

    Task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ */
+void print_number(int n)
+{
+	unsigned int c;
+
+	if (n < 0)
+	{
+		c = -n;
+		_putchar('-');
+	} else
+	{
+		c = n;
+	}
+
+	if (c / 10)
+	{
+		print_number(c / 10);
+	}
+
+	_putchar((c % 10) + '0');
+}
