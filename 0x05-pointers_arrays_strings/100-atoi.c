dc9ba073e8520617581a9158747323fa066a173f
Rim Goundal <goundalrim@gmail.com>
2023-02-27 05:35:57 -0800

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,34 @@
 #include "main.h"
+
 /**
- * _atoi - Entry point
- * @s: input
- * Return: 0
+ * _atoi - function with one argument
+ * @s: char type pointer argument
+ * Description: convert a string to an integer
+ * Return: value of s
  */
 
 int _atoi(char *s)
 {
+	int i, mist, comb;
 
-	return (0);
+	i = 0;
+	mist = 1;
+	comb = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			mist *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			while (s[i] >= '0' && s[i] <= '9')
+			{
+				comb = (s[i] - '0') * mist + comb * 10;
+				i++;
+			}
+			break;
+		}
+		i++;
+	}
+	return (comb);
 }
