fb961943c8a28e6cb2a960041df5ea2b42f99e9d
Rim Goundal <goundalrim@gmail.com>
2023-03-06 03:42:57 -0800

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,43 +1,24 @@
 #include "main.h"
+#include <stddef.h>
 /**
- * _strstr - prints the consecutive caracters of s1 that are in s2.
- * @haystack: source string
- * @needle: searching string
- * Return: new string.
+ * _strstr - searches string for a substring
+ * @haystack: string to search
+ * @needle: the substring to find
+ * Return: pointer to first matching substring or NULL
  */
 
 char *_strstr(char *haystack, char *needle)
 {
+	int a;
+
 	while (*haystack)
 	{
-		if ((*haystack == *needle && coincidence(haystack, needle) == 1) || !*needle)
-		{
+		for (a = 0; *(needle + a); a++)
+			if (*(needle + a) != *(haystack + a))
+				break;
+		if (!*(needle + a))
 			return (haystack);
-		}
-		else
-		{
-			haystack++;
-		}
+		haystack++;
 	}
-	return (0);
-}
-/**
- * coincidence - define if the string b is inside a.
- * @a: source string
- * @b: string to be searched
- * Return: 1 if there is coincidence, otherwise 0.
- */
-
-int coincidence(char *a, char *b)
-{
-	while (*b && *b == *a)
-	{
-		b++;
-		a++;
-	}
-
-	if (*b == '\0')
-		return (1);
-	else
-		return (0);
+	return (NULL);
 }
