cb28b33f070e9b2cb93ae630aae3ed9b1f597882
Rim Goundal <goundalrim@gmail.com>
2023-03-22 04:03:54 -0700

    Task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,6 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#include <stdlib.h>
-
-/* print_name : Write a function that prints a name.*/
 void print_name(char *name, void (*f)(char *))
 
 #endif
